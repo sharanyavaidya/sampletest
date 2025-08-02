08724691c0803272f13bdae34d14af8a3fa4bef2
sharanyavaidya <sharanyavaidya9@gmail.com>
2025-08-02 12:05:08 +0530

    first update

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask 
-app =Flask(__name__)
+from flask import Flask
+app = Flask(__name__)
+
 @app.route("/")
 def index():
-    return "Welcome to MRECW"
+    return "Hello"
+@app.route("/home")
+def home():
+    return "Home Page"
 if __name__=="__main__":
     app.run(host='0.0.0.0',port=1000)
