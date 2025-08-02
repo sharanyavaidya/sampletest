129559108c4a4354abe354dc47b9bf038e50510c
sharanyavaidya <sharanyavaidya9@gmail.com>
2025-08-02 13:47:23 +0530

    second update

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask,render_template
+
+app = Flask(_name_)
 
 @app.route("/")
 def index():
-    return "Hello"
+    return render_template("index.html")
 @app.route("/home")
 def home():
-    return "Home Page"
-if __name__=="__main__":
-    app.run(host='0.0.0.0',port=1000)
+    return "home page"
+if _name_ == "_main_":
+    app.run(host='0.0.0.0', port=10000)
